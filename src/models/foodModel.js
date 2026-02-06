22300314667d2275987c7c54fdb83a18eca1171f
Yasmin Porto <yasmin.porto@aluno.senai.br>
2026-02-06 16:19:54 -0300

    Subindo model

--- src/models/foodModel.js
+++ src/models/foodModel.js
@@ -1,39 +1,42 @@
 import prisma from '../utils/prismaClient.js';
 
 export const create = async (data) => {
-    return await prisma.exemplo.create({ data });
+    return await prisma.food.create({ data });
 };
 
 export const findAll = async (filters = {}) => {
-    const { nome, descricao, ano, preco } = filters;
+    const { name, description, price, category } = filters;
     const where = {};
 
-    if (nome) where.nome = { contains: nome, mode: 'insensitive' };
-    if (descricao) where.descricao = { contains: descricao, mode: 'insensitive' };
-    if (ano !== undefined) where.ano = parseInt(ano);
-    if (preco !== undefined) where.preco = parseFloat(preco);
+    if (name) where.name = { contains: name, mode: 'insensitive' };
+    if (description) where.description = { contains: description, mode: 'insensitive' };
+    if (price !== undefined) where.price = parseInt(price);
+    if (category !== undefined) where.category = parseFloat(category);
 
-    return await prisma.exemplo.findMany({
+    return await prisma.food.findMany({
         where,
         orderBy: { createdAt: 'desc' },
     });
 };
 
 export const findById = async (id) => {
-    return await prisma.exemplo.findUnique({
+    return await prisma.food.findUnique({
         where: { id: parseInt(id) },
     });
 };
 
 export const update = async (id, data) => {
-    return await prisma.exemplo.update({
+    return await prisma.food.update({
         where: { id: parseInt(id) },
         data,
     });
 };
 
 export const remove = async (id) => {
-    return await prisma.exemplo.delete({
+    return await prisma.food.delete({
         where: { id: parseInt(id) },
     });
 };
+
+
+
